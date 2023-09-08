3c9dac57e7fd7915f56b0d054b806a1e608bde2f
Adam Monroe <composedevelopment@gmail.com>
2023-09-08 12:17:42 -0400

    Changed config for Github Pages

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,14 @@ module.exports = {
     entry: './script.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
     },
     stats: {
         warnings: false
     },
     devServer: {
         static: {
-            directory: path.join(__dirname, './dist'),
+            directory: path.join(__dirname, './docs'),
         },
         client: {
             overlay: {
